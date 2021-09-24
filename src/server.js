7c7538184a3540746de34f380cb5b08981d50ab8
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-09-25 00:27:14 +0300

    add static asset hosting

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const path = require('path');
 
 const initHandlebars = require('./config/handlebars.js');
 
 const app = express();
 
 initHandlebars(app);
+
+// require('./config/handlebars.js')(app);
+
+app.use(express.static(path.resolve(__dirname, './static')));
 
 app.all('/', (req, res) => {
     res.render('index');
