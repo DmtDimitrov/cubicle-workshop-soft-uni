6d16dd02da8b2afb48c6a439678c1b3acafa8b7d
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-09-26 00:04:25 +0300

    add no result functionality

--- src/controllers/homeContoller.js
+++ src/controllers/homeContoller.js
@@ -24,7 +24,13 @@ const search = (req, res) => {
 
     let cubes = cubeService.search(search, from, to);
 
-    res.render('index', { cubes });
+    res.render('index', { 
+        cubes,
+        title: 'SEARCH',
+        search, 
+        from, 
+        to
+    });
 }
 
 router.get('/', homeView);
