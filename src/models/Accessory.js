3519044f1d8897392f6c88bfe5c677030d6e05fa
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-10-03 00:32:30 +0300

    Create accessory model

---
+++ src/models/Accessory.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const accessorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true,
+        maxlength: 50
+    },
+    imageUrl: {
+        type: String,
+        required: [true, 'Image url is required!'],
+        validate: [/^https?:\/\//i, 'invalid image url!']
+    }
+});
+
+const Accessory = mongoose.model('Accessory', accessorySchema);
+
+module.exports = Accessory;
