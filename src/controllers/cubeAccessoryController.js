e0562581d2d6f77bf6474bfac591dca5d09d22cb
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-10-03 04:23:15 +0300

    show accessory select on accessory add view

--- src/controllers/cubeAccessoryController.js
+++ src/controllers/cubeAccessoryController.js
@@ -1,12 +1,14 @@
 const router = require('express').Router({ mergeParams: true });
 
-const cubeService = require('../services/cubeService.js')
+const cubeService = require('../services/cubeService.js');
+const accessoryService = require('../services/accessoryService.js');
 
 router.get('/add', async (req, res) => {
     try {
         let cube = await cubeService.getOne(req.params.cubeId);
+        let accessories = await accessoryService.getAll();
 
-        res.render('cube/accessory/add', { ...cube });
+        res.render('cube/accessory/add', { cube, accessories });
     } catch (error) {
         res.status(400).send(error.message);
     }
