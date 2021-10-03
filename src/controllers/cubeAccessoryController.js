27f94f8d59140e8db9ecf19d4354097f65b9cf0a
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-10-03 03:25:24 +0300

    set up accessory add

--- src/controllers/cubeAccessoryController.js
+++ src/controllers/cubeAccessoryController.js
@@ -1,8 +1,16 @@
-const router = require('express').Router({mergeParams: true});
+const router = require('express').Router({ mergeParams: true });
 
-router.get('/add', (req, res) => {
-    console.log(req.params.cubeId);
-    res.end();
+const cubeService = require('../services/cubeService.js')
+
+router.get('/add', async (req, res) => {
+    try {
+        let cube = await cubeService.getOne(req.params.cubeId);
+
+        res.render('cube/accessory/add', { ...cube });
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+
 });
 
 module.exports = router;
