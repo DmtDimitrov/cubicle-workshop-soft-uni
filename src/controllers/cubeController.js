0b08bc19b4f7d97150d9a31a1bf504591cb911de
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-10-03 18:38:05 +0300

    Show accessories on details view

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -27,6 +27,7 @@ const createCube = async (req, res) => {
 const cubeDetails = async (req, res) => {
     try {
         let cube = await cubeService.getOne(req.params.cubeId);
+        
         res.render('cube/details', { ...cube });
     } catch (error) {
 
